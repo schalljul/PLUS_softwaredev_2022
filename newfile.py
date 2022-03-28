61f77b2a091e421fba8f518af8a082837e348172
Julian <101421287+schalljul@users.noreply.github.com>
2022-03-28 11:25:58 +0200

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Test
+"""
+import os
+import sys
+
+#123 test
+print(sys.path)
+help("modules")
+
+days_of_the_week = 7
+def say_hello (recipient):
+    print("Hello, world! Hello {} ".format(recipient))
+    return recipient
